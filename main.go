13c993239797da809ea12ce47bf7d94eae2dd28d
alfnsnff <alfnsnff@gmail.com>
2024-10-03 00:07:14 +0700

    add setup database cloud to dockerfile

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
     "github.com/labstack/echo/v4"
-    // "GOstarted/database"
-	// "GOstarted/seeds"
+    "GOstarted/database"
+	"GOstarted/seeds"
 	"GOstarted/routes"
 )
 
 func main() {
     
-    // database.Connect()
-    // seeds.SeedShips()
+    database.Connect()
+    seeds.SeedShips()
 
     e := echo.New()
 	routes.SetupRoutes(e)
