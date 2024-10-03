b0320742d3367ce36c1ab86ada548ee277d02310
alfnsnff <alfnsnff@gmail.com>
2024-10-03 12:57:04 +0700

    Setup ENV VARS

--- database/database.go
+++ database/database.go
@@ -6,36 +6,38 @@ import (
 	"os"
 
 	"gorm.io/gorm"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"GOstarted/models"
 )
 
 var DB *gorm.DB
 
+// Connect initializes the database connection
 func Connect() {
 	var err error
 
-	// Load .env file
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	// Load .env file only for local development (if necessary)
+	if os.Getenv("ENV") == "development" {
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
-	// Local development using localhost PostgreSQL
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=%s", 
-		os.Getenv("DB_HOST"), 
-		os.Getenv("DB_USER"), 
-		os.Getenv("DB_PASSWORD"), 
+	// Prepare the DSN based on environment variables
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),     // for Cloud SQL, this should be the socket path
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"))
 
+	// Open a database connection using Gorm
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
 
-
-	// Automatically migrate the schema for the Well model
+	// Automatically migrate the schema for the defined models
 	err = DB.AutoMigrate(&models.Ship{}, &models.Harbor{}, &models.User{}, &models.Ticket{}, &models.TicketClass{}, &models.Pathway{})
 	if err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
